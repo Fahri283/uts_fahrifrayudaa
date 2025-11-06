3d12f564e168c37cd98d392c57f03d99cf9b9285
fahri frayuda <frayudafahri@gmail.com>
2025-11-06 09:42:11 +0700

    penambahan perhitungan total

---
+++ dashboard(perhitungantotal).php
@@ -0,0 +1,10 @@
+// Hitung total harga dan akumulasi grand total
+    $grandtotal += $total;
+
+    $beli[] = [
+        "nama" => $nama,
+        "harga" => $harga,
+        "jumlah" => $kuantitas,
+        "total" => $total
+    ];
+}
