8370bc4a592d92fd194c8026b4f06d5ae36e4f72
fahri frayuda <frayudafahri@gmail.com>
2025-11-06 09:40:13 +0700

    penambahan setup awal

---
+++ dashboard(setupawl).php
@@ -0,0 +1,10 @@
+<?php
+// Daftar nama barang & harga dipisah
+$namaBarang = ["celak", "lipstik", "Parfum", "sampo", "Bedak"];
+$hargaBarang = [3000, 5000, 50000, 350000, 15000];
+
+echo "<h1>--POLGAN MART--</h1>";
+
+// Array untuk simpan hasil belanja
+$beli = [];
+$grandtotal = 0;
